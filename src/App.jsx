f7e8c8c590044ac72d3c5c669ff5b8e43aba783d
zafarahmedbaloch102_fat <zafarahmedbaloch102@gmail.com>
2026-04-06 17:57:01 -1200

    Formated the code for better readability and added comments to explain the  changes made in the commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,73 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [todoList, setTodoList]= useState([]);
-  let saveToDoList= (event)=>{
-
+  let [todoList, setTodoList] = useState([]);
+  let saveToDoList = (event) => {
     let toname = event.target.toname.value.trim();
-    if(!toname) return;
-    let isDuplicate = todoList.some(item=>item.toLowerCase()===toname.toLowerCase());
-    if(!isDuplicate)
-    {
-     let finalList = [...todoList, toname];
-     setTodoList(finalList);
+    if (!toname) return;
+    let isDuplicate = todoList.some(
+      (item) => item.toLowerCase() === toname.toLowerCase(),
+    );
+    if (!isDuplicate) {
+      let finalList = [...todoList, toname];
+      setTodoList(finalList);
+    } else {
+      alert("Todo List already exist ....");
     }
-    else{
-      alert("Todo List already exist ....")
-    }
-    event.target.toname.value='';
+    event.target.toname.value = "";
     event.preventDefault();
   };
 
-  let list =todoList.map((item, index)=>{
-    return(
-      <TodoListItem value= {item} key={index} indexNumber = {index} todoList={todoList}
-       setTodoList={setTodoList} 
-       
-       />
-    )
-  })
+  let list = todoList.map((item, index) => {
+    return (
+      <TodoListItem
+        value={item}
+        key={index}
+        indexNumber={index}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
+    );
+  });
 
   return (
-   <div className="App  mt-20">
-    <h1 className='text-[50px] font-bold text-center '>ToDo List</h1>
-    <form onSubmit= {saveToDoList}>
-      <input  type='text' name='toname' className='border border-black rounded-md'/> 
-      <button className='border border-black bg-blue-500 text-white rounded-md'>save</button>
-    </form>
+    <div className="App  mt-20">
+      <h1 className="text-[50px] font-bold text-center ">ToDo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input
+          type="text"
+          name="toname"
+          className="border border-black rounded-md"
+        />
+        <button className="border border-black bg-blue-500 text-white rounded-md">
+          save
+        </button>
+      </form>
 
-    <div className='outerDiv '>
-    <ul>
-      {list}
-     
-    </ul>
+      <div className="outerDiv ">
+        <ul>{list}</ul>
+      </div>
     </div>
-   </div>
-  )
+  );
 }
-export default App
+export default App;
 
-function TodoListItem({value, indexNumber, todoList, setTodoList}){
-   
-  let [staus, setSatus]= useState(false)
-  let deleteRow = ()=>{
-    let finalData= todoList.filter((v,i)=>i!==indexNumber)
-    setTodoList(finalData)
-  }
-  let checkStatus = ()=>{
-    setSatus(!staus)
-  }
-  return(
-    <li className={(staus) ? 'completetodo': ''}
-     onClick={checkStatus}> {indexNumber+1 }{value}
-      <span  onClick={deleteRow}>&times;</span></li>
-  
-  )}
+function TodoListItem({ value, indexNumber, todoList, setTodoList }) {
+  let [staus, setSatus] = useState(false);
+  let deleteRow = () => {
+    let finalData = todoList.filter((v, i) => i !== indexNumber);
+    setTodoList(finalData);
+  };
+  let checkStatus = () => {
+    setSatus(!staus);
+  };
+  return (
+    <li className={staus ? "completetodo" : ""} onClick={checkStatus}>
+      {" "}
+      {indexNumber + 1}
+      {value}
+      <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
+}
